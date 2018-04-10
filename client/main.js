b43366c1549be638bfcf5dd33ac05af6078b73d2
Ross Wojcik <rosswojcik@RossMacbookPro.local>
2018-04-10 14:33:59 -0500

    updated python code

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var socket = io.connect('http://10.10.90.78:3000'); // create connection
+    var socket = io.connect('http://localhost:3000'); // create connection
     socket.on('valuesReceived', function(data) {
         $('#air-quality').text(data.airQuality);
 //        $('#orientation').text(data.orientation);
--- python/SLAPP_Project.py
+++ python/SLAPP_Project.py
@@ -1,30 +1,3 @@
-
-'''
-## License
-
-The MIT License (MIT)
-
-GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
-Copyright (C) 2017  Dexter Industries
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-'''
 import time
 import requests
 import grovepi
@@ -33,20 +6,27 @@ import sys
 # A0
 # SIG,NC,VCC,GND
 air_sensor = 0
+dist_sensor = 4
 
 url = "http://10.10.90.78:3000"
 
 while True:
     try:
-        sensor_value = grovepi.analogRead(air_sensor)
+        if grovepi.analogRead(air_sensor) <= 500:
+            sensor_value = 'Good'
+        else:
+            sensor_value = 'Bad'
+            
+        
+    #    dist_value = grovepi.analogRead(dist_sensor)
 
-        print(sensor_value)
+     #   print(sensor_value)
 
-        payload = { 'airQuality' : sensor_value }
+        payload = { 'airQuality' : sensor_value}
 
         requests.post(url, data=payload)
 
-        time.sleep(0.1)
+        time.sleep(1)
 
     except TypeError:
         print ("Error")
