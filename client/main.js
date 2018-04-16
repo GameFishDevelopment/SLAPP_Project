836d875b0db70373b3f950bbcee6785b41461082
Ross Wojcik <rosswojcik@RossMacbookPro.local>
2018-04-16 11:03:27 -0500

    Move repo to SLAPP from SLAPP_Wojcik

--- client/main.js
+++ client/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    var socket = io.connect('http://localhost:3000'); // create connection
+    var socket = io.connect('localhost:3000'); // create connection
     socket.on('valuesReceived', function(data) {
         $('#air-quality').text(data.airQuality);
+        $('#distance').text(data.distance);
+        $('#humidity').text(data.humi);
+        $('#temperature').text(data.temp);
+        $('#light-sensor').text(data.lightValue);
 //        $('#orientation').text(data.orientation);
 //        $('#vibration').text(data.vibration);
 //        $('#accelerometer').text(data.accelerometer);
